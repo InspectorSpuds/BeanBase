12e99e7701039eb857d38d875d4a6777d8944e4c
InspectorSpuds <ishanparikh18@gmail.com>
2023-12-15 22:56:00 -0800

    revised frontend organization for redesign

---
+++ old_frontend/README.md
@@ -0,0 +1,8 @@
+## Frontend
+
+This is the website frontend, run with npm start
+
+
+## Logo 
+
+Periodic table element
---
+++ old_frontend/build/precache-manifest.63d44ec9676c8921139869a45e942add.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b0b599b66d2d383bdeb",
+    "url": "/static/css/main.39472ddf.chunk.css"
+  },
+  {
+    "revision": "1b0b599b66d2d383bdeb",
+    "url": "/static/js/main.9dc36e0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e55fdb2e680edda58b6",
+    "url": "/static/js/2.d4971e5c.chunk.js"
+  },
+  {
+    "revision": "5822e8ba0ab27d8d615dd8064425d09d",
+    "url": "/index.html"
+  }
+];
